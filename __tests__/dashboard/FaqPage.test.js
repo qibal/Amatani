0de9b3efb15fc7b65bc35bd18a02ca7a9a7ebdf1
qibal <iqbalherlambang.39@gmail.com>
2025-01-20 01:46:59 +0700

    update test jest

--- __tests__/dashboard/FaqPage.test.js
+++ __tests__/dashboard/FaqPage.test.js
@@ -22,6 +22,18 @@ global.fetch = jest.fn((url, options) => {
     return Promise.reject(new Error('Unknown endpoint'));
 });
 
+window.matchMedia = jest.fn().mockImplementation(query => {
+    return {
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    };
+});
 describe('FaqPage', () => {
     beforeEach(() => {
         fetch.mockClear();
