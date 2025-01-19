bebd9bb109f0cd19baa7f157e3d496665074506f
qibal <iqbalherlambang.39@gmail.com>
2025-01-19 10:13:13 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
                 pathname: '/**',
                 search: '',
             },
+            {
+                protocol: 'https:',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+                search: '',
+            },
         ],
     },
 };
